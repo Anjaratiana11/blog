87186189cd0c45d6bba927c63a52d2da94535536
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-03-18 20:04:35 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Découvrez les dernières tendances du digital et du design graphique. Articles, analyses et conseils SEO pour les créateurs de contenu.">
+    <meta name="keywords" content="digital, design, tendances, SEO, communication, graphisme">
+    <meta name="author" content="Designova Team">
+    <title>Designova - Blog Digital & Design</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <p>Hello</p>
+</body>
+</html>

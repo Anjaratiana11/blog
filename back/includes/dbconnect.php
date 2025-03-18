221fe850ace10c1d114cb7b1c6384c528031ce84
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-03-18 22:39:15 +0300

    no base yet

--- back/includes/dbconnect.php
+++ back/includes/dbconnect.php
@@ -6,8 +6,8 @@ $password = "root";
 $dbname = "designova";   
 
 // Création de la connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+// $conn = new mysqli($servername, $username, $password, $dbname);
+$conn=null;
 // Vérifier si la connexion est réussie
 if ($conn->connect_error) {
     die("Connexion échouée : " . $conn->connect_error);

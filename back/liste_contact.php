b93109c6dcd30021e27c42c44cfa1013fdadda95
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2025-03-19 09:48:46 +0300

    last update require

--- back/liste_contact.php
+++ back/liste_contact.php
@@ -1,5 +1,5 @@
 <?php 
-include_once 'includes/fonctionsBack.php';    // Les fonctions CRUD (findAllContact)
+include_once 'includes/FonctionsBack.php';    // Les fonctions CRUD (findAllContact)
 
 $contacts = findAllContact();  // Récupérer tous les contacts
 ?>
